b8f4555741e42174278d7ae2b4580bc353dea2f8
devkyoshi <hgp.ashi@gmail.com>
2026-03-01 18:12:08 +0530

    implemented product-service and gateway

---
+++ backend/item-service/src/main/java/com/ctse/itemservice/model/Item.java
@@ -0,0 +1,29 @@
+package com.ctse.itemservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
